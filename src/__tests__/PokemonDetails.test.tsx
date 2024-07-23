cf1031802c1b3da3f2f79c6ca150676d0a48933f
Maxim <maximozaitsev@gmail.com>
2024-07-23 19:48:22 +0400

    fix: comment react imports to fix terminal issues

--- src/__tests__/PokemonDetails.test.tsx
+++ src/__tests__/PokemonDetails.test.tsx
@@ -1,5 +1,5 @@
 // src/__tests__/PokemonDetails.test.tsx
-import React from 'react';
+// import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import PokemonDetails from '../components/PokemonDetails';
--- src/__tests__/PokemonList.test.tsx
+++ src/__tests__/PokemonList.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import axios from 'axios';
