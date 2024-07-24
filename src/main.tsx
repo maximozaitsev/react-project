e1c332405a25bce529552741ef8d32434d90ae42
Maxim <maximozaitsev@gmail.com>
2024-07-24 16:27:00 +0400

    refactor: move ErrorBoundary to components folder

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary';
 import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
