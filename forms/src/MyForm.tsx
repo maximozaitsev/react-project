8b21a4d379c22354e35e1b27b5728ae48801b281
Maxim <maximozaitsev@gmail.com>
2024-08-15 18:54:35 +0800

    feat: integrate Yup

--- forms/src/MyForm.tsx
+++ forms/src/MyForm.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup.object().shape({
+  firstName: yup.string().required('First name is required'),
+  lastName: yup.string().required('Last name is required'),
+  email: yup
+    .string()
+    .email('Invalid email address')
+    .required('Email is required'),
+})
 
 type FormData = {
   firstName: string
@@ -12,9 +23,11 @@ const MyForm: React.FC = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>()
+  } = useForm<FormData>({
+    resolver: yupResolver(schema), // Connect Yup with React Hook Form
+  })
 
-  const onSubmit = (data: FormData) => {
+  const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data)
   }
 
@@ -22,26 +35,17 @@ const MyForm: React.FC = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          {...register('firstName', { required: 'First name is required' })}
-        />
+        <input id="firstName" {...register('firstName')} />
         {errors.firstName && <span>{errors.firstName.message}</span>}
       </div>
       <div>
         <label htmlFor="lastName">Last Name</label>
-        <input
-          id="lastName"
-          {...register('lastName', { required: 'Last name is required' })}
-        />
+        <input id="lastName" {...register('lastName')} />
         {errors.lastName && <span>{errors.lastName.message}</span>}
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          {...register('email', { required: 'Email is required' })}
-        />
+        <input id="email" {...register('email')} />
         {errors.email && <span>{errors.email.message}</span>}
       </div>
       <button type="submit">Submit</button>
